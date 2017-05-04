61074c3341605ef434fa0b67331496da4b83df62
darekbl <daro477@interia.pl>
2017-05-04 20:58:22 +0200

    General features are done.

---
+++ BinaryClock/Clock.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace BinaryClock
+{
+    class Clock
+    {
+        public int hour = DateTime.Now.Hour;
+        public int minutes = DateTime.Now.Minute;
+
+        public long byteHour = DateTime.Now.ToBinary();
+
+
+        public bool[] getBinaryHour()
+        {
+            hour = DateTime.Now.Hour;
+
+            int rest = hour;
+            bool[] binaryHour = new bool[4];
+
+
+            /*
+             * Preparing hour
+             */
+            if (hour % 12 > 0)
+            {
+                hour = hour % 12;
+            }
+
+            int h = 8;
+            int i = 0;
+
+            while (i <= 3)
+                    {
+                        if (hour/h >= 1)
+                            {
+                                binaryHour[i] = true;
+                                hour = hour % h;
+                                Console.WriteLine(i + "<-- Reszta --- Tablica: --> " + binaryHour[i]);
+                            }
+                            else
+                                {
+                                    binaryHour[i] = false;
+                                    Console.WriteLine("ELSE HOUR -- iteracja -->" + i);
+                                }
+
+                        i++;
+                        h = h/2;
+                        Console.WriteLine($"h{i} = {h}", h, i);
+                    }
+
+            return binaryHour;
+        }
+
+        public bool[] getBinaryMinutes()
+        {
+            minutes = DateTime.Now.Minute;
+
+            int rest = minutes;
+            bool[] binaryMinutes = new bool[6];
+
+            int m = 32;
+            int i = 0;
+
+            while (i <= 5)
+            {
+                Console.WriteLine("min/m = " + minutes / m);
+                if (minutes / m >= 1)
+                {
+                    binaryMinutes[i] = true;
+                    minutes = minutes % m;
+                    Console.WriteLine(i + "<-- Reszta --- Tablica: --> "+  binaryMinutes[i]);
+                }
+                else
+                {
+                    binaryMinutes[i] = false;
+                    Console.WriteLine("ELSE MINUTES -- iteracja -->" + i);
+                }
+
+                i++;
+                m = m / 2;
+            }
+
+            return binaryMinutes;
+        }
+
+        public bool isPM()
+        {
+            if (DateTime.Now.Hour % 12 > 0)
+                return true;
+            else
+                return false;
+        }
+    }
+}
--- BinaryClock/MainWindow.xaml.cs
+++ BinaryClock/MainWindow.xaml.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -23,6 +26,102 @@ namespace BinaryClock
         public MainWindow()
         {
             InitializeComponent();
+            Clock TIMER = new Clock();
+
+            /*
+             * Variables
+             */
+            var label = (Label)FindName("godzinaLabel");
+
+            var h8 = (Ellipse)this.FindName("h8");
+            var h4 = (Ellipse)this.FindName("h4");
+            var h2 = (Ellipse)this.FindName("h2");
+            var h1 = (Ellipse)this.FindName("h1");
+
+            var m32 = (Ellipse)this.FindName("m32");
+            var m16 = (Ellipse)this.FindName("m16");
+            var m8 = (Ellipse)this.FindName("m8");
+            var m4 = (Ellipse)this.FindName("m4");
+            var m2 = (Ellipse)this.FindName("m2");
+            var m1 = (Ellipse)this.FindName("m1");
+
+            var PM = (Ellipse)this.FindName("PM");
+
+            List<Ellipse> ellipsesHour = new List<Ellipse>
+            {
+                h8,h4,h2,h1
+            };
+
+            List<Ellipse> ellipsesMinutes = new List<Ellipse>
+            {
+                m32,m16,m8,m4,m2,m1
+            };
+            
+            /*
+             * Get the hour and minutes in binary style
+             */
+            bool[] binaryHour = TIMER.getBinaryHour();
+            bool[] binaryMinutes = TIMER.getBinaryMinutes();
+            /*
+             * Set the ellipses
+             */
+             
+            setBinaryHour(binaryHour, ellipsesHour);
+            setBinaryHour(binaryMinutes, ellipsesMinutes);
+            
+            setPM(TIMER.isPM());
+
+            /*
+             * Set the digital clock
+             */
+            label.Content = "Godzina: " + DateTime.Now.ToShortTimeString();
+            
+        }
+
+        private void setBinaryHour(bool[] binaryHour, List<Ellipse> list)
+        {
+            int i = 0;
+            foreach(var ellipse in list)
+            {
+                if(binaryHour[i] == true)
+                {
+                    ellipse.Fill = Brushes.ForestGreen;
+                }
+                else
+                {
+                    ellipse.Fill = Brushes.White;
+                }
+                i++;
+            }
+        }
+
+        private void setBinaryMinutes(bool[] binaryMinutes, List<Ellipse> list)
+        {
+            int i = 0;
+            foreach (var ellipse in list)
+            {
+                if (binaryMinutes[i] == true)
+                {
+                    ellipse.Fill = Brushes.ForestGreen;
+                }
+                else
+                {
+                    ellipse.Fill = Brushes.White;
+                }
+                i++;
+            }
+        }
+
+        private void setPM(Boolean isPM)
+        {
+            if (isPM == true)
+            {
+                PM.Fill = Brushes.Coral;
+            }
+            else
+            {
+                PM.Fill = Brushes.White;
+            }
         }
     }
 }
